bc11887a684fd41eb19b16a44cf91d8e5b600e1f
rseed42 <petkov.venelin@gmail.com>
2015-08-02 10:01:02 +0200

    Show various world mazes

--- tools/showmaze.py
+++ tools/showmaze.py
@@ -1,7 +1,12 @@
+import sys
 import numpy
 import matplotlib.pyplot as pyplot
 
-z = numpy.load(file('data/world/maze.geom','rb'))
+fn = sys.argv[1]
+if not fn:
+    sys.stderr.write('Need path to file\n')
+    sys.exit(1)
+z = numpy.load(file(fn,'rb'))
 # Show
 pyplot.figure(figsize=(10, 5))
 pyplot.imshow(z,cmap=pyplot.cm.binary, interpolation='nearest')
