f1d3b26bd4d1636e7f7ef8e25939dde7e54c5d6c
rseed42 <petkov.venelin@gmail.com>
2015-08-02 22:35:21 +0200

    removed old code

--- sim/controller.py
+++ sim/controller.py
@@ -13,7 +13,6 @@ WDC_DOWN  = 0x8
 class UserController(object):
     def __init__(self, agent):
         self.agent = agent
-#        print self.agent.controlState
 
     def keyDown(self, sb):
         if   sb == sdl.SDLK_a:
@@ -25,17 +24,6 @@ class UserController(object):
         elif sb == sdl.SDLK_s:
             self.agent.controlState |= WDC_DOWN
 
-#        if sb == sdl.SDLK_w:
-#            self.agent.accelerate()
-#        if sb == sdl.SDLK_k:
-#            self.agent.reverse()
-#        if sb == sdl.SDLK_s:
-#            self.agent.brake()
-#        if sb == sdl.SDLK_a:
-#            self.agent.steerLeft()
-#        if sb == sdl.SDLK_d:
-#            self.agent.steerRight()
-
     def keyUp(self, sb):
         if   sb == sdl.SDLK_a:
             self.agent.controlState &= ~WDC_LEFT
@@ -45,14 +33,3 @@ class UserController(object):
             self.agent.controlState &= ~WDC_UP
         elif sb == sdl.SDLK_s:
             self.agent.controlState &= ~WDC_DOWN
-
-#        if sb == sdl.SDLK_w:
-#            self.agent.releaseAccelerator()
-#        if sb == sdl.SDLK_k:
-#            self.agent.releaseReverse()
-#        if sb == sdl.SDLK_s:
-#            self.agent.releaseBrake()
-#        if sb == sdl.SDLK_a:
-#            self.agent.releaseSteering()
-#        if sb == sdl.SDLK_d:
-#            self.agent.releaseSteering()
