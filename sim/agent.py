c5b2fa69c2bba907e743d5fd18fc614963ea456e
rseed42 <petkov.venelin@gmail.com>
2015-08-02 16:26:02 +0200

    New wheel dynamics test

--- sim/agent.py
+++ sim/agent.py
@@ -3,6 +3,101 @@ import numpy as np
 from staticobject import StaticObject
 from mind import programed
 #-------------------------------------------------------------------------------
+# Wheel
+#-------------------------------------------------------------------------------
+WDC_LEFT  = 0x1
+WDC_RIGHT = 0x2
+WDC_UP    = 0x4
+WDC_DOWN  = 0x8
+#-------------------------------------------------------------------------------
+class Wheel(object):
+    def __init__(self):
+        self.body = None
+        self.maxForwardSpeed = 0
+        self.maxBackwardSpeed = 0
+        self.maxDriveForce = 0
+
+    def create(self, world):
+        bodyDef = b2.b2BodyDef()
+        bodyDef.type = b2.b2_dynamicBody
+        bodyDef.position = (22,20)
+        self.body = world.CreateBody(bodyDef)
+#        polygonShape = b2.b2PolygonShape()
+#        polygonShape.SetAsBox(0.5,1.25)
+        fx = self.body.CreatePolygonFixture(box=(0.5, 1.25), density=1)
+        # Density = 1
+#        self.body.CreateFixture(polygonShape, 1)
+
+    def setCharacteristics(self, forward, back, drive):
+        self.maxForwardSpeed = forward
+        self.maxBackwardSpeed = back
+        self.maxDriveForce = drive
+
+    def getLateralVelocity(self):
+        currentRightNormal = self.body.GetWorldVector((1,0))
+        return b2.b2Dot(currentRightNormal, self.body.linearVelocity) * currentRightNormal
+
+    def getForwardVelocity(self):
+        currentForwardNormal = self.body.GetWorldVector((0,1))
+        return b2.b2Dot(currentForwardNormal, self.body.linearVelocity) * currentForwardNormal
+
+    def updateFriction(self):
+        # Lateral linear velocity
+        maxLateralImpulse = 2.5
+        impulse = self.body.mass * -self.getLateralVelocity()
+        if impulse.length > maxLateralImpulse:
+            impulse *= maxLateralImpulse / impulse.length
+        self.body.ApplyLinearImpulse(impulse, self.body.worldCenter, True)
+        # Angular velocity
+        self.body.ApplyAngularImpulse(0.1*self.body.inertia*-self.body.angularVelocity, True)
+        # Forward linear velocity
+        currentForwardNormal = self.getForwardVelocity()
+        currentForwardSpeed = currentForwardNormal.Normalize()
+        dragForceMagnitude = -2 * currentForwardSpeed
+        self.body.ApplyForce(dragForceMagnitude * currentForwardNormal,
+                             self.body.worldCenter, True)
+
+#    def updateDrive(self, controlState):
+    def updateDrive(self, controlState):
+#        print bin(controlState)
+#        return
+        desiredSpeed = 0
+#        print bin(self.controlState)
+#        return
+        if controlState & WDC_UP: desiredSpeed = self.maxForwardSpeed
+        if controlState & WDC_DOWN: desiredSpeed = self.maxBackwardSpeed
+#        else: return
+        print desiredSpeed
+        # Current speed in forward direction
+        currentForwardNormal = self.body.GetWorldVector((0,1))
+        currentSpeed = b2.b2Dot(self.getForwardVelocity(), currentForwardNormal)
+        # Apply necessary force
+        force = 0
+        if desiredSpeed > currentSpeed:
+            force = self.maxDriveForce
+        elif desiredSpeed < currentSpeed:
+            force = -self.maxDriveForce
+        else:
+            return
+        self.body.ApplyForce(force*currentForwardNormal,
+                             self.body.worldCenter,
+                             True)
+
+#    def updateTurn(self, controlState):
+    def updateTurn(self, controlState):
+        desiredTorque = 0
+#        print self.controlState
+#        return
+        if controlState & (WDC_LEFT|WDC_RIGHT):
+            if controlState & WDC_LEFT:
+                desiredTorque = 15
+            elif controlState & WDC_RIGHT:
+                desiredTorque = -15
+            else:
+                pass
+        self.body.ApplyTorque(desiredTorque, True)
+
+#-------------------------------------------------------------------------------
 # Agent
 #-------------------------------------------------------------------------------
 class Agent(object):
@@ -32,6 +127,10 @@ class Agent(object):
         # Mind
         self.mind = None
 
+        self.controlState = 0x0
+
+        self.wheel = Wheel()
+
     def addWheel(self, world, carBody, carPos, cfg):
         wheelDef = b2.b2BodyDef()
         wheelDef.type = b2.b2_dynamicBody
@@ -48,11 +147,11 @@ class Agent(object):
 
     def addFrontWheel(self, world, carBody, carPos, cfg):
         wheel = self.addWheel(world, carBody, carPos, cfg)
-        jointDef = b2.b2RevoluteJointDef()
-        jointDef.Initialize(carBody, wheel, wheel.worldCenter)
-        jointDef.enableMotor = True
-        jointDef.maxMotorTorque = cfg.max_motor_torque
-        joint = world.CreateJoint(jointDef)
+#        jointDef = b2.b2RevoluteJointDef()
+#        jointDef.Initialize(carBody, wheel, wheel.worldCenter)
+#        jointDef.enableMotor = True
+#        jointDef.maxMotorTorque = cfg.max_motor_torque
+#        joint = world.CreateJoint(jointDef)
         return wheel
 
     def addRearWheel(self, world, carBody, carPos, cfg):
@@ -84,6 +183,7 @@ class Agent(object):
 
     def construct(self, world, cfg):
         # Initialize the control variables
+        pass
         self.max_engine_speed = cfg.max_engine_speed
         self.max_steer_angle = (b2.b2_pi/180)*cfg.max_steer_angle
         self.steering_speed = cfg.steering_speed
@@ -117,9 +217,12 @@ class Agent(object):
         self.rearRightWheel = self.addRearWheel(world, self.body,
                                                   cfg.position,
                                                   cfg.wheels.rearRight)
+        # Wheel
+        self.wheel.create(world)
+        self.wheel.setCharacteristics(100, -20, 150)
+        self.frontLeftWheel = self.wheel.body
         # Create Sensor
         self.addSensor(world, cfg.sensor)
-
         # Create the mind or remain mindless
         if not cfg.mind: return
         self.mind = programed.MindProgram()
@@ -160,34 +263,55 @@ class Agent(object):
     def killOrthogonalVelocity(self, body):
         """ This function applies a "friction" in a direction orthogonal to the
             body's axis. """
-        localPoint = b2.b2Vec2(0,0)
-        velocity = body.GetLinearVelocityFromLocalPoint(localPoint)
-        sidewaysAxis = body.transform.R.col2
-        sidewaysAxis *= b2.b2Dot(velocity, sidewaysAxis)
-        body.linearVelocity = sidewaysAxis
+#        localPoint = b2.b2Vec2(0,0)
+#        velocity = body.GetLinearVelocityFromLocalPoint(localPoint)
+#        sidewaysAxis = body.transform.R.col2
+#        sidewaysAxis *= b2.b2Dot(velocity, sidewaysAxis)
+#        body.linearVelocity = sidewaysAxis
+
 
     def update(self):
         # Decision-making time
         if self.mind: self.mind.think()
-
         # Put our actions into physics
-        self.killOrthogonalVelocity(self.frontLeftWheel)
-        self.killOrthogonalVelocity(self.frontRightWheel)
-        self.killOrthogonalVelocity(self.rearLeftWheel)
-        self.killOrthogonalVelocity(self.rearRightWheel)
-        # Driving
-        lforce = self.frontLeftWheel.transform.R.col2 * self.engineSpeed
-        self.frontLeftWheel.ApplyForceToCenter(lforce, True)
-        rforce = self.frontRightWheel.transform.R.col2 * self.engineSpeed
-        self.frontRightWheel.ApplyForceToCenter(rforce, True)
-        # Steering
-        lj = self.frontLeftWheel.joints[0].joint
-        mspeed = self.steeringAngle - lj.angle
-        lj.motorSpeed = mspeed * self.steering_speed
+        self.wheel.updateFriction()
+        self.wheel.updateDrive(self.controlState)
+        self.wheel.updateTurn(self.controlState)
 
-        rj = self.frontRightWheel.joints[0].joint
-        mspeed = self.steeringAngle - rj.angle
-        rj.motorSpeed = mspeed * self.steering_speed
+#        body = self.frontLeftWheel
+#        currentRightNormal = body.GetWorldVector((1,0))
+#        lateralVelocity = b2.b2Dot(currentRightNormal, body.linearVelocity) * currentRightNormal
+
+#        currentForwardNormal = body.GetWorldVector((0,1))
+#        forwardVelocity = b2.b2Dot(currentForwardNormal, body.linearVelocity) * currentForwardNormal
+
+#        impulse = body.mass * -lateralVelocity
+#        body.ApplyLinearImpulse(impulse, body.worldCenter,True)
+#        body.ApplyAngularImpulse(0.1*body.inertia*-body.angularVelocity,True)
+#        forwardSpeed = forwardVelocity.Normalize()
+#        dragForceMagnitude = -2 * forwardSpeed
+#        body.ApplyForce(dragForceMagnitude*currentForwardNormal, body.worldCenter, True)
+
+
+#        self.killOrthogonalVelocity(self.frontLeftWheel)
+#        self.killOrthogonalVelocity(self.frontRightWheel)
+#        self.killOrthogonalVelocity(self.rearLeftWheel)
+#        self.killOrthogonalVelocity(self.rearRightWheel)
+       # Driving
+#        lforce = self.frontLeftWheel.transform.R.col2 * self.engineSpeed
+#        self.frontLeftWheel.ApplyForceToCenter(lforce, True)
+#        rforce = self.frontRightWheel.transform.R.col2 * self.engineSpeed
+#        self.frontRightWheel.ApplyForceToCenter(rforce, True)
+#        # Steering
+
+
+#        lj = self.frontLeftWheel.joints[0].joint
+#        mspeed = self.steeringAngle - lj.angle
+#        lj.motorSpeed = mspeed * self.steering_speed
+#
+#        rj = self.frontRightWheel.joints[0].joint
+#        mspeed = self.steeringAngle - rj.angle
+#        rj.motorSpeed = mspeed * self.steering_speed
 
     def handleCollisionBegin(self, contact, fixtureMe, fixtureOther):
         """ Collision events that do not come from the sensor,
--- sim/controller.py
+++ sim/controller.py
@@ -1,5 +1,10 @@
 import sdl2 as sdl
 #-------------------------------------------------------------------------------
+WDC_LEFT  = 0x1
+WDC_RIGHT = 0x2
+WDC_UP    = 0x4
+WDC_DOWN  = 0x8
+#-------------------------------------------------------------------------------
 # Agent Controller
 #-------------------------------------------------------------------------------
 """ The controller translates the actions of the user/mind to physical responses
@@ -8,27 +13,46 @@ import sdl2 as sdl
 class UserController(object):
     def __init__(self, agent):
         self.agent = agent
+#        print self.agent.controlState
 
     def keyDown(self, sb):
-        if sb == sdl.SDLK_w:
-            self.agent.accelerate()
-        if sb == sdl.SDLK_k:
-            self.agent.reverse()
-        if sb == sdl.SDLK_s:
-            self.agent.brake()
-        if sb == sdl.SDLK_a:
-            self.agent.steerLeft()
-        if sb == sdl.SDLK_d:
-            self.agent.steerRight()
+        if   sb == sdl.SDLK_a:
+            self.agent.controlState |= WDC_LEFT
+        elif sb == sdl.SDLK_d:
+            self.agent.controlState |= WDC_RIGHT
+        elif sb == sdl.SDLK_w:
+            self.agent.controlState |= WDC_UP
+        elif sb == sdl.SDLK_s:
+            self.agent.controlState |= WDC_DOWN
+
+#        if sb == sdl.SDLK_w:
+#            self.agent.accelerate()
+#        if sb == sdl.SDLK_k:
+#            self.agent.reverse()
+#        if sb == sdl.SDLK_s:
+#            self.agent.brake()
+#        if sb == sdl.SDLK_a:
+#            self.agent.steerLeft()
+#        if sb == sdl.SDLK_d:
+#            self.agent.steerRight()
 
     def keyUp(self, sb):
-        if sb == sdl.SDLK_w:
-            self.agent.releaseAccelerator()
-        if sb == sdl.SDLK_k:
-            self.agent.releaseReverse()
-        if sb == sdl.SDLK_s:
-            self.agent.releaseBrake()
-        if sb == sdl.SDLK_a:
-            self.agent.releaseSteering()
-        if sb == sdl.SDLK_d:
-            self.agent.releaseSteering()
+        if   sb == sdl.SDLK_a:
+            self.agent.controlState &= ~WDC_LEFT
+        elif sb == sdl.SDLK_d:
+            self.agent.controlState &= ~WDC_RIGHT
+        elif sb == sdl.SDLK_w:
+            self.agent.controlState &= ~WDC_UP
+        elif sb == sdl.SDLK_s:
+            self.agent.controlState &= ~WDC_DOWN
+
+#        if sb == sdl.SDLK_w:
+#            self.agent.releaseAccelerator()
+#        if sb == sdl.SDLK_k:
+#            self.agent.releaseReverse()
+#        if sb == sdl.SDLK_s:
+#            self.agent.releaseBrake()
+#        if sb == sdl.SDLK_a:
+#            self.agent.releaseSteering()
+#        if sb == sdl.SDLK_d:
+#            self.agent.releaseSteering()
